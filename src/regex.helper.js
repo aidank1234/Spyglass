50af77f5441e8be48e7950781b309b6a6a8b3037
Indigo <indigoethsec@gmail.com>
2023-09-23 19:55:10 -0400

    Added UI changes for regex detector type

--- src/regex.helper.js
+++ src/regex.helper.js
@@ -1,4 +1,4 @@
 export async function matchRegex(codeTomatch, regexPattern) {
 	const pattern = new RegExp(regexPattern);
-	return pattern.test(codeTomatch);
+	return pattern.test(codeTomatch) ? "Yes" : "No";
 }
