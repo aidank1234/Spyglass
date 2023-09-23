5c387359a7ddb88fa4b53daba19d9a5449d1d408
Aidan Kaiser <apkaiser@wisc.edu>
2023-09-23 11:19:54 -0400

    everything tracked in state variables

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [codeEntry, setCodeEntry] = useState('');
   const [selectedValue, setSelectedValue] = useState("AI");
+  const [apiKey, setApiKey] = useState('');
+  const [prompt, setPrompt] = useState('');
+  const [output, setOutput] = useState('');
 
   const handleSelectChange = (event) => {
     setSelectedValue(event.target.value);
@@ -11,21 +15,47 @@ function App() {
   return (
     <div className="mainContainer">
       <div className='horizontalPart'>
-        <textarea className='codeEntry' placeholder='Enter code here...'></textarea>
+        <textarea 
+          className='codeEntry' 
+          placeholder='Enter code here...'
+          value={codeEntry}
+          onChange={(e) => setCodeEntry(e.target.value)}
+        ></textarea>
       </div>
 
       <div className='horizontalPart verticallyCentered'>
-        <select value={selectedValue} onChange={handleSelectChange}>
+        <select 
+          value={selectedValue} 
+          onChange={handleSelectChange}
+        >
           <option value="AI">AI</option>
         </select>
 
         {selectedValue === "AI" && (
-          <input type="text" className="apiKeyInput" placeholder="API key" />
+          <input 
+            type="text" 
+            className="apiKeyInput" 
+            placeholder="API key"
+            value={apiKey}
+            onChange={(e) => setApiKey(e.target.value)}
+          />
         )}
 
-        <textarea className="promptInput" placeholder="Prompt"></textarea>
+        <textarea
+          className="promptInput"
+          placeholder="Prompt"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+        ></textarea>
+        
         <button className="runButton">Run</button>
-        <textarea className="outputBox" placeholder="Output"></textarea>
+        
+        <textarea 
+          className="outputBox" 
+          placeholder="Output"
+          value={output}
+          onChange={(e) => setOutput(e.target.value)}
+        ></textarea>
       </div>
     </div>
   );
