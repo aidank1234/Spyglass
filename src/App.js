bb7b2b901fdae25dc65f1211a9fd225c7b3c5c6f
Aidan Kaiser <apkaiser@wisc.edu>
2023-09-23 11:12:44 -0400

    UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="mainContainer">
-      <div className='horizontalPart'></div>
-      <div className='horizontalPart'></div>
+      <div className='horizontalPart'>
+        <textarea placeholder="Enter your code here..." className="codeEntry"></textarea>
+      </div>
+      <div className='horizontalPart'>
+        <select defaultValue="AI">
+          <option value="AI">AI</option>
+        </select>
+        <textarea placeholder="Prompt" className="promptInput"></textarea>
+        <button className="runButton">Run</button>
+        <textarea placeholder="Output" className="outputBox"></textarea>
+      </div>
     </div>
   );
 }
